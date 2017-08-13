f22da370f424df82f2e877f07d1f3495e03f03af
id-gue <gue@danke.fish>
2017-08-13 20:36:26 +0200

    added classes MinimumBalanceAccount and RationalNumber

--- oop/oop.py
+++ oop/oop.py
@@ -49,22 +49,90 @@ in the shell:
 
 # 2__ Classes and Objects
 
-# simple class
+# simple account
 class BankAccount:
     def __init__(self):
         self.balance = 0
 
-    def class_withdraw(self, amount):
+    def withdraw(self, amount):
         self.balance -= amount
         return self.balance
 
-    def class_deposit(self, amount):
+    def deposit(self, amount):
         self.balance += amount
         return self.balance
 
 """
 in the shell:
     a = BankAccount()
-    a.class_deposit(10)
-    b.class_withdraw(10)
+    a.deposit(10)
+    a.withdraw(10)
 """
+
+# 3__ Inheritance
+
+
+# more sophisicated account with minimum balance
+class MinimumBalanceAccount(BankAccount):
+    def __init__(self, minimum_balance):
+        BankAccount.__init__(self)
+        self.minimum_balance = minimum_balance
+
+    # overwrites withdraw from BankAccount
+    def withdraw(self, amount):
+        if self.balance - amount < self.minimum_balance:
+            print ('Sorry, minimum balance must be maintained.')
+        else:
+            BankAccount.withdraw(self, amount)
+
+"""
+in the shell:
+    a = MinimumBalanceAccount(10)
+    a.deposit(10)
+    a.withdraw(10)
+"""
+
+# 4__ Special Class Methods
+
+
+class RationalNumber:
+    def __init__(self, nummerator, denominator=1):
+        self.n = nummerator
+        self.d = denominator
+
+    def __add__(self, other):
+        if not isinstance(other, RationalNumber):
+            other = RationalNumber(other)
+
+        n = self.n * other.d + self.d * other.n
+        d = self.d * other.d
+        return RationalNumber(n, d)
+
+    def __sub__(self, other):
+        if not isinstance(other, RationalNumber):
+            other = RationalNumber(other)
+
+        n1, d1 = self.n, self.d
+        n2, d2 = other.n, other.d
+        return RationalNumber(n1 * d2 - n2 * d1, d1 * d2)
+
+    def __mul__(self, other):
+        if not isinstance(other, RationalNumber):
+            other = RationalNumber(other)
+
+        n1, d1 = self.n, self.d
+        n2, d2 = other.n, other.d
+        return RationalNumber(n1 * n2, d1 * d2)
+
+    def __div__(self, other):
+        if not isinstance(other, RationalNumber):
+            other = RationalNumber(other)
+
+        n1, d1 = self.n, self.d
+        n2, d2 = other.n, other.d
+        return RationalNumber(n1 * d2, d1 * n2)
+
+    def __str__(self):
+        return "%s/%s" % (self.n, self.d)
+
+    __repr__ = __str__
