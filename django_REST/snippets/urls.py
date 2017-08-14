ba9a91c701c1ae5066dde5bccbce4a804d7c07ec
id-gue <gue@danke.fish>
2017-08-14 20:56:23 +0200

    started with REST framework tutorial chapter 7 / added a schema view

--- django_REST/snippets/urls.py
+++ django_REST/snippets/urls.py
@@ -1,6 +1,20 @@
 from django.conf.urls import url, include
 from snippets import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
+# Schema describes available api endpoints, their urls & operations
+#   >> auto-generates documentation
+#   >> drive client libraries that interacts with apis
+
+# requirement: $ pip install coreapi
+
+# adding a schema
+schema_view = get_schema_view(title='Pastebin API')
+
+urlpatterns = [
+    url(r'^schema/$', schema_view),
+]
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
