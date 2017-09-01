6d87293625cc10a10eba8f9bb2f5c575c91381ce
mddemarie <mddemarie@gmail.com>
2017-09-01 11:22:58 +0200

    more practicing querysets and OOP

--- queryset_practice/quickstart/models.py
+++ queryset_practice/quickstart/models.py
@@ -8,10 +8,19 @@ class Role(models.Model):
         return self.name
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=40)
+    house_number = models.CharField(max_length=40)
+    post_code = models.IntegerField()
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=20)
+
+
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     roles = models.ManyToManyField(Role, related_name="people")
+    address = models.ForeignKey(Address, null=True)
 
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
